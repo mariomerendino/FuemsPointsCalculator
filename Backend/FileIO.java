79e68786b51eb8606a08b55585ab0ceecd00da04
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-10-15 22:09:23 -0400

    added cooments

--- Backend/FileIO.java
+++ Backend/FileIO.java
@@ -2,15 +2,23 @@ package Backend;
 
 import java.util.*;
 import java.io.*;
-public class FileIO{ 
-    public HashMap<String, Integer> numPointsMap = new HashMap<String, Integer>();
-    public String ErrorLog = "";
+public class FileIO{
+
+    //HashMap containing Names, and points
+    private HashMap<String, Integer> numPointsMap = new HashMap<String, Integer>();
+    //error log String
+    public static String ErrorLog = "<html><br>";
+    //current line Number
     private int lineNum;
+
+    //Runs through the CSV File, and adds people and points to 
+    //the hashmap. 
     public void CalculatePoints(String filePath){
+        numPointsMap = new HashMap<String, Integer>();
+        ErrorLog = "<html><br>";
         String[] s;
-        Person temp = new Person();
         String key = new String();
-        lineNum = 0;
+        lineNum = 1;
         String line; 
         try{
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
@@ -20,22 +28,22 @@ public class FileIO{
                 lineNum++;
                 try{
                     s = line.split(",");
-                    temp.fName = s[3];
-                    temp.lName = s[2];
-                    key = temp.fName+","+temp.lName;
+                    
+                    key = s[3]+","+s[2];
                     key = key.replaceAll("\\s","");
 
                     if(numPointsMap.containsKey(key)){
-                        numPointsMap.put(key, numPointsMap.get(key) + NumPointsForActivity(s));
+                        numPointsMap.put(key, numPointsMap.get(key) + NumPointsForActivity(s, lineNum));
 
                     }
                     else{
-                        numPointsMap.put(key, NumPointsForActivity(s));
+                        numPointsMap.put(key, NumPointsForActivity(s, lineNum));
                         
                     }
                 }
                 catch(Exception e){
-                    PrintToError("Error");
+                
+                    PrintToError("Error: line (" + Integer.toString(lineNum) + ")");
                 }
                 
             }
@@ -44,12 +52,20 @@ public class FileIO{
             PrintToError("Error Reading file");
         }
     }
-    public int NumPointsForActivity(String[] line){
+    //Used to determine the amount of points for each entry 
+    public int NumPointsForActivity(String[] line, int lineNum){
         String activityName = line[5];
-        System.out.println(activityName);
         int ans = 0;
         if(activityName.equals("Standby")){
             ans = 1;
+            try{
+                double numberOfHours = Double.parseDouble(line[8]);
+                
+            }
+            catch(Exception e){
+                PrintToError("Error: Line (" + Integer.toString(lineNum) + ") Hours are not integers. Points not added" );
+                return 0;
+            }
         }
         else if(activityName.equals("CPR Class")){
             ans = 1; 
@@ -84,6 +100,10 @@ public class FileIO{
         return ans;
     }
 
+    //Returns the total amount of points each student as a string in the format of:
+    //Name: John Smith
+    //Points: 1
+    //endl. 
     public String GetPoints(){
         String ans = "";
         String temp; 
@@ -101,13 +121,13 @@ public class FileIO{
             }
             it.remove(); 
         }
-        System.out.println(ans);
         return ans;
     }
-
+    //Print to error log.
     public void PrintToError(String message){
-        ErrorLog =  message + "\n";
+        ErrorLog =  ErrorLog + message + "<br>";
     }
+    //returns the file extention of any inputed file. 
     public String getFileExtension(File file) {
         String name = file.getName();
         try {
--- Frontend/UI.java
+++ Frontend/UI.java
@@ -13,24 +13,38 @@ import java.io.IOException;
 //RUN WITH
 //java UI
 public class UI extends JFrame{
+
+    //Instance of File IO class, for file interaction
     FileIO io;
     
+    //Selected CSV File
     File selectedFile;
+    String filePath;
+
+    //Swing Components.
     JButton calculate;
     JButton openFile;
     JButton error;
-    String filePath;
+    JButton clear;
     ImageIcon img;
     JLabel points;
     JLabel status;
     JTextArea area;
     JScrollPane areaScrollPane;
+    
+    //Default Constructor
+    //Sets up JFrame 
     public UI(){
+        //instantiates file io
         io = new FileIO();
+        //Jframe Setup. 
+        setSize(700,450);
         setTitle("Fuems Point's Calculator");
         setLayout(new BorderLayout(10, 10));
         img = new ImageIcon(getClass().getResource("/icon.png"));
         this.setIconImage(img.getImage());
+
+        //Checks if its a mac, if so, change doc icon
         if ( exists( "com.apple.eawt.Application" ) )
         {
             try{
@@ -40,65 +54,100 @@ public class UI extends JFrame{
                 io.PrintToError("Cannot load Doc Icon.");
             }
         }
+        //Sets up text area with scroll bar
+        //(Not added to the main Jframe until CenterPanel() is called)
         area = new JTextArea(20,50);
         areaScrollPane = new JScrollPane(area);
         areaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        
-        areaScrollPane.setPreferredSize(new Dimension(500, 300));
+        areaScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        areaScrollPane.setSize(new Dimension(400, 300));
         area.setText(" ");
         area.setEditable(false);
-        setVisible(true);
-        setSize(600,400);
+
+        //add North, center and south panels to the main Jframe
         add(NorthPanel(), BorderLayout.NORTH);
         add(centerPanel(), BorderLayout.CENTER);
         add(southPanel(), BorderLayout.SOUTH);
         
+        setVisible(true);        
     }
+    //Returns the North Panel. 
     public JPanel NorthPanel(){
-        JPanel North = new JPanel();        
+        //Creates Jpanel
+        JPanel North = new JPanel();  
+
+        //setup Buttons on north panel
         openFile = new JButton("Open File");        
         calculate = new JButton("Calculate Points");
-        calculate.setVisible(false);        
+        
+        calculate.setEnabled(false);
+        //Calculate is clicked        
         calculate.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Calculate the points for the selected file
                 io.CalculatePoints(selectedFile.getAbsolutePath());
+                //set the text of the main text area
                 area.setText(io.GetPoints());
+                //disable the calculate button again
+                calculate.setEnabled(false);
+                clear.setEnabled(true);
             }
         });
+        //Open file is clicked 
         openFile.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Open file picker
                 pickFile();
-                calculate.setVisible(true);
+                
                 
             }
         });
+        //Sets the layout of the north panel
         North.setLayout(new FlowLayout());
 
         ImageIcon imageIcon = new ImageIcon(img.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT));
-
         JLabel icon = new JLabel();
         icon.setIcon(imageIcon);
+
+        //add all the componenets to the north panel
         North.add(icon);
         North.add(openFile);
         North.add(calculate);
         return North;
     }
+    //Creates south panel
     public JPanel southPanel(){
         JPanel south = new JPanel();
+        //sets up error button
         error = new JButton("Error Log");
+        //error button is pressed
+        error.setEnabled(false);
         error.addActionListener(new ActionListener() { 
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Popup window with the error log
                 JFrame popup = errorLog();
             }
         });
+        //sets up clear button
+        clear = new JButton("Clear");
+        clear.setEnabled(false);        
+        clear.addActionListener(new ActionListener() { 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearAll();
+            }
+        });
+        south.add(clear);        
         south.add(error);
         return south;
     }
+    //File picker prompt window
     public void pickFile(){
         JFileChooser fileChooser = new JFileChooser();
+        //Sets filter for only CSV files
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Comma Seperated Values", "csv", "csv");
         fileChooser.setFileFilter(filter);
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
@@ -106,8 +155,14 @@ public class UI extends JFrame{
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
             System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+            calculate.setEnabled(true);
+            error.setEnabled(true);
+            clear.setEnabled(true);
+            openFile.setEnabled(false);
         }
     }
+    //Helper function for setting up the apple doc.
+    //Checks what kind of machine 
     public boolean exists(String className)
     {
         try {
@@ -118,23 +173,30 @@ public class UI extends JFrame{
             return false;
         }
     }
+    //Error log popup
     public JFrame errorLog(){
         JFrame panel = new JFrame(); 
         panel.setTitle("Fuems Point's Calculator Error Log");
         panel.setLayout(new BorderLayout(10, 10));
         panel.setVisible(true);
         panel.setSize(600,400);
+        //Gets the error log fromthe file Io class
         panel.add(new Label(io.ErrorLog),BorderLayout.CENTER);
         return panel;
     }
     public JPanel centerPanel(){
         JPanel center = new JPanel();
         center.setVisible(true);
-        
-        //JScrollPane scrollPane = new JScrollPane(area, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         center.add(areaScrollPane);
-        
         return center;        
+    }
+    public void clearAll(){
+        clear.setEnabled(false);
+        calculate.setEnabled(false);
+        error.setEnabled(false);        
+        openFile.setEnabled(true);
+        io.ErrorLog = "<html><br>";
+        area.setText(" ");   
     }
     
 }
